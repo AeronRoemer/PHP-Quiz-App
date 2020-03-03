9507469f6768a0a6ccfee34c101e53dcc938bdbe
AeronRoemer <57688724+AeronRoemer@users.noreply.github.com>
2020-03-02 19:31:48 -0700

    Minor updates

--- index.php
+++ index.php
@@ -13,20 +13,20 @@
 </head>
 <body>
 <?php
-$total_questions = 10;
+//sets session variable basics
 if (isset($_POST['page'])){
     $q_num = filter_input(INPUT_POST, 'page', FILTER_VALIDATE_INT);
-    if ($q_num >= 9){
-        $q_num = "Session Over";
+    if ($q_num >= 10){
+        $q_num = 0;
         session_destroy();
     }
 } else {
 $q_num = 0;
 }
-
+//store session information in two arrays - question location information and answers given. 
 $_SESSION['wrongAnswer'][$q_num-1] = filter_input(INPUT_POST, 'answer', FILTER_VALIDATE_INT);
 $_SESSION['correctAnswer'][$q_num-1] = filter_input(INPUT_POST, 'correctAnswer', FILTER_VALIDATE_INT);
-//var_dump($_SESSION);
+//remove NULL values for shortened arrays. 
 $wrongs = array_filter($_SESSION['wrongAnswer']);
 $rights = array_filter($_SESSION['correctAnswer']);
 ?>
@@ -41,16 +41,21 @@ $rights = array_filter($_SESSION['correctAnswer']);
             <form action="index.php" method="POST">
                 <input type="hidden" name="page" value="<?=$q_num+1?>" />
                 <?php 
-                //creates and randomizes answers buttons
+                //creates and randomizes answers buttons.
                    $answers[] = '<input type="submit" class="btn" name="correctAnswer" value="' . $questions[$q_num]['correctAnswer'] . '" />';
                    $answers[] = '<input type="submit" class="btn" name="answer" value="' . $questions[$q_num]['firstIncorrectAnswer'] . '" />';
                    $answers[] = '<input type="submit" class="btn" name="answer" value="' . $questions[$q_num]['secondIncorrectAnswer'] . '" />';
                 shuffle($answers);
                 echo $answers[0] . " " . $answers[1] . " " . $answers[2];
-                echo "<br>You have " . sizeof($rights) . " answers correct & " . sizeof($wrongs) . " answers wrong.";
-                echo "<br>";
                 ?>
             </form>
+            <div id="count">
+              <?php
+                 //displays numbers right & wrong. 
+                echo "<br><br>You have " . sizeof($rights) . " answers correct & " . sizeof($wrongs) . " answers wrong.";
+                  echo "<br>";
+                 ?>
+            </div>
         </div>
     </div>
 </body>
