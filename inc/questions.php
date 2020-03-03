370102f4ce6db03893431f8f5b5e719af96801cf
AeronRoemer <57688724+AeronRoemer@users.noreply.github.com>
2020-03-02 19:04:35 -0700

    Session counts right/wrong answers

--- inc/questions.php
+++ inc/questions.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 // initialize questions variable
 $questions = [];
 function generate_question(){
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-    include('inc/quiz.php');
+    include('inc/questions.php');
     session_start();
      ?>
 <!DOCTYPE html>
@@ -12,23 +12,43 @@
     <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
+<?php
+$total_questions = 10;
+if (isset($_POST['page'])){
+    $q_num = filter_input(INPUT_POST, 'page', FILTER_VALIDATE_INT);
+    if ($q_num >= 9){
+        $q_num = "Session Over";
+        session_destroy();
+    }
+} else {
+$q_num = 0;
+}
 
+$_SESSION['wrongAnswer'][$q_num-1] = filter_input(INPUT_POST, 'answer', FILTER_VALIDATE_INT);
+$_SESSION['correctAnswer'][$q_num-1] = filter_input(INPUT_POST, 'correctAnswer', FILTER_VALIDATE_INT);
+//var_dump($_SESSION);
+$wrongs = array_filter($_SESSION['wrongAnswer']);
+$rights = array_filter($_SESSION['correctAnswer']);
+?>
 
     <div class="container">
         <div id="quiz-box">
-            <p class="breadcrumbs">Question #<?= $q_num;?> of <?= $total_questions;?></p>
+            <p class="breadcrumbs">Question #<?= $q_num + 1;?> of <?= $total_questions;?></p>
             <p class="quiz">What is <?php 
+            //displays question from array based on question number
            echo $questions[$q_num]["leftAdder"] . " + " . $questions[$q_num]["rightAdder"] . "?"
             ?> </p>
             <form action="index.php" method="POST">
                 <input type="hidden" name="page" value="<?=$q_num+1?>" />
                 <?php 
                 //creates and randomizes answers buttons
-                   $answers[] = '<input type="submit" class="btn" name="answer" value="' . $questions[$q_num]['correctAnswer'] . '" />';
+                   $answers[] = '<input type="submit" class="btn" name="correctAnswer" value="' . $questions[$q_num]['correctAnswer'] . '" />';
                    $answers[] = '<input type="submit" class="btn" name="answer" value="' . $questions[$q_num]['firstIncorrectAnswer'] . '" />';
-                   $answers[] = '<input type="submit" class="btn" name="answer" value="' . $questions[$q_num]['firstIncorrectAnswer'] . '" />';
+                   $answers[] = '<input type="submit" class="btn" name="answer" value="' . $questions[$q_num]['secondIncorrectAnswer'] . '" />';
                 shuffle($answers);
-                echo $answers[0] . " " . $answers[1] . " " . $answers[2]
+                echo $answers[0] . " " . $answers[1] . " " . $answers[2];
+                echo "<br>You have " . sizeof($rights) . " answers correct & " . sizeof($wrongs) . " answers wrong.";
+                echo "<br>";
                 ?>
             </form>
         </div>
